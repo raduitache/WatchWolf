cde8d028d98779a10b2a29606dfd8e10fb0f7fc7
reloadedd <reloadedd@protonmail.com>
2022-05-23 19:25:24 +0300

    Disable encryption for S3 buckets because money

--- infrastructure/firehose/main.tf
+++ infrastructure/firehose/main.tf
@@ -23,23 +23,6 @@ resource "aws_s3_bucket_public_access_block" "block_public_access" {
   ignore_public_acls      = true
 }
 
-resource "aws_kms_key" "s3_encryption_key" {
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 7
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
-  bucket = aws_s3_bucket.bucket.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.s3_encryption_key.arn
-      sse_algorithm     = "aws:kms"
-    }
-    bucket_key_enabled = true
-  }
-}
-
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.tags["Project"]}-${var.tags["Environment"]}-${var.tags["Creator"]}-${local.app_role}-bucket"
 }
