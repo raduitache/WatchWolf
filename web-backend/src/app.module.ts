c09ee01ce2ebc914121769b2757fca79afb8dbf3
Radu Ioan Tache <radutache76@gmail.com>
2022-05-24 05:34:54 +0300

    Everything I've worked on so far

--- web-backend/src/app.module.ts
+++ web-backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['process.env', 'dist/process.env'],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
